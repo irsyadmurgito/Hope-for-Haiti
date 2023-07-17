0ddeb4d7dc69ab0d876bcef69fbb024548dfde2c
irsyadmurgito <136702794+irsyadmurgito@users.noreply.github.com>
2023-07-17 07:53:06 +0700

    Improve data filtering

---
+++ jungle_house_river.kt
@@ -0,0 +1,657 @@
+//1
+fun hopeForHaiti() {
+    println("Hope for Haiti!")
+}
+
+//2
+fun donateMoney() {
+    println("Donate money to Haiti relief funds!")
+}
+
+//3
+fun prayForHaiti() {
+    println("Pray for Haiti!")
+}
+
+//4
+fun raiseAwareness() {
+    println("Raise awareness for Haiti!")
+}
+
+//5
+fun encourageAction() {
+    println("Encourage action for Haiti!")
+}
+
+//6
+fun volunteerYourTime() {
+    println("Volunteer your time for Haiti!")
+}
+
+//7
+fun contactPoliticians() {
+    println("Contact politicians about Haiti!")
+}
+
+//8
+fun provideMedicalAid() {
+    println("Provide medical aid to Haiti!")
+}
+
+//9
+fun donateSupplies() {
+    println("Donate medical supplies to Haiti!")
+}
+
+//10
+fun joinAnOrganization() {
+    println("Join an organization to help Haiti!")
+}
+
+//11
+fun attendEvents() {
+    println("Attend events to support Haiti relief!")
+}
+
+//12
+fun educateYourself() {
+    println("Educate yourself about Haiti!")
+}
+
+//13
+fun donateClothes() {
+    println("Donate clothes to support Haiti!")
+}
+
+//14
+fun contributeToEfforts() {
+    println("Contribute to Hurricane Matthew relief efforts!")
+}
+
+//15
+fun spreadTheWord() {
+    println("Spread the word about urgent needs in Haiti!")
+}
+
+//16
+fun supportOrganizations() {
+    println("Support organizations that help Haiti!")
+}
+
+//17
+fun getInvolvedWithConnections() {
+    println("Get involved with Haiti connections!")
+}
+
+//18
+fun joinAReliefCampaign() {
+    println("Join a relief campaign for Haiti!")
+}
+
+//19
+fun donateBooks() {
+    println("Donate books to help rebuild Haiti!")
+}
+
+//20
+fun purchaseProducts() {
+    println("Purchase products to help Haitians!")
+}
+
+//21
+fun donateToNGOs() {
+    println("Donate to NGOs working in Haiti!")
+}
+
+//22
+fun donateToCharities() {
+    println("Donate to charities working in Haiti!")
+}
+
+//23
+fun signPetitions() {
+    println("Sign petitions for Haiti!")
+}
+
+//24
+fun researchWhatYouCanDo() {
+    println("Research what you can do to help Haiti!")
+}
+
+//25
+fun contactLocalOrganizations() {
+    println("Contact local organizations to help Haiti!")
+}
+
+//26
+fun engageInConversations() {
+    println("Engage in conversations about Haiti!")
+}
+
+//27
+fun shopForAGoodCause() {
+    println("Shop for a good cause and support Haiti!")
+}
+
+//28
+fun helpWithRebuilding() {
+    println("Help with rebuilding Haiti after the earthquake!")
+}
+
+//29
+fun supportLocalBusinesses() {
+    println("Support local businesses in Haiti!")
+}
+
+//30
+fun donateToCrowdfundingPlatforms() {
+    println("Donate to crowdfunding platforms for Haiti!")
+}
+
+//31
+fun volunteerRemotely() {
+    println("Volunteer remotely to help Haiti!")
+}
+
+//32
+fun signUpToTutoringPlatforms() {
+    println("Sign up to tutoring platforms to help Haitians!")
+}
+
+//33
+fun hostAFundraiser() {
+    println("Host a fundraiser to support Haiti!")
+}
+
+//34
+fun engageYourNetwork() {
+    println("Engage your network in supporting Haiti!")
+}
+
+//35
+fun fundraiseOnline() {
+    println("Fundraise online to support Haiti relief!")
+}
+
+//36
+fun shareStoriesAboutHaiti() {
+    println("Share stories about Haiti on social media!")
+}
+
+//37
+fun donateTimeToHelp() {
+    println("Donate your time to help Haiti!")
+}
+
+//38
+fun writeToPoliticalLeaders() {
+    println("Write to political leaders to focus on Haiti!")
+}
+
+//39
+fun supportEntrepreneurs() {
+    println("Support entrepreneurs in Haiti!")
+}
+
+//40
+fun investInSmallBusinesses() {
+    println("Invest in small businesses in Haiti!")
+}
+
+//41
+fun createCampaigns() {
+    println("Create campaigns to spread awareness about Haiti!")
+}
+
+//42
+fun sponsorASchool() {
+    println("Sponsor a school in Haiti!")
+}
+
+//43
+fun donateToSchools() {
+    println("Donate to schools in Haiti!")
+}
+
+//44
+fun contributeToSchools() {
+    println("Contribute to schools in Haiti!")
+}
+
+//45
+fun advocateForHaiti() {
+    println("Advocate for Haiti!")
+}
+
+//46
+fun supportCleanEnergyProjects() {
+    println("Support clean energy projects in Haiti!")
+}
+
+//47
+fun joinSocialMovements() {
+    println("Join social movements for Haiti!")
+}
+
+//48
+fun getInformedAboutHaiti() {
+    println("Get informed about Haiti!")
+}
+
+//49
+fun supportArtsAndCulture() {
+    println("Support arts and culture in Haiti!")
+}
+
+//50
+fun investInEducation() {
+    println("Invest in education in Haiti!")
+}
+
+//51
+fun helpRebuildHouses() {
+    println("Help rebuild houses in Haiti!")
+}
+
+//52
+fun provideMentalHealthAssistance() {
+    println("Provide mental health assistance in Haiti!")
+}
+
+//53
+fun improveAccessToWater() {
+    println("Improve access to water in Haiti!")
+}
+
+//54
+fun donateFood() {
+    println("Donate food to Haiti!")
+}
+
+//55
+fun advocateForHumanRights() {
+    println("Advocate for human rights in Haiti!")
+}
+
+//56
+fun supportInitiatives() {
+    println("Support initiatives to reduce poverty in Haiti!")
+}
+
+//57
+fun joinProtests() {
+    println("Join protests for Haiti!")
+}
+
+//58
+fun supportArtists() {
+    println("Support Haitian artists!")
+}
+
+//59
+fun donateToHaitianOrganizations() {
+    println("Donate to Haitian organizations!")
+}
+
+//60
+fun investInTechnology() {
+    println("Invest in technology development in Haiti!")
+}
+
+//61
+fun helpNegotiatePeace() {
+    println("Help negotiate peace in Haiti!")
+}
+
+//62
+fun supportMicrofinanceOrganizations() {
+    println("Support microfinance organizations in Haiti!")
+}
+
+//63
+fun implementSustainableDevelopmentGoals() {
+    println("Implement sustainable development goals in Haiti!")
+}
+
+//64
+fun raiseFunds() {
+    println("Raise funds for Haiti!")
+}
+
+//65
+fun supportHaitianFarmers() {
+    println("Support Haitian farmers!")
+}
+
+//66
+fun provideLegalAid() {
+    println("Provide legal aid to Haitians!")
+}
+
+//67
+fun contributeScientificKnowledge() {
+    println("Contribute scientific knowledge to Haiti!")
+}
+
+//68
+fun inspireOthers() {
+    println("Inspire others to help Haiti!")
+}
+
+//69
+fun supportSmallBusinesses() {
+    println("Support small businesses in Haiti!")
+}
+
+//70
+fun provideFinancialSupport() {
+    println("Provide financial support for Haiti!")
+}
+
+//71
+fun volunteerInHaiti() {
+    println("Volunteer in Haiti!")
+}
+
+//72
+fun donateCellPhones() {
+    println("Donate cell phones to Haiti!")
+}
+
+//73
+fun donateComputers() {
+    println("Donate computers to Haiti!")
+}
+
+//74
+fun helpWithFinancialLiteracy() {
+    println("Help with financial literacy in Haiti!")
+}
+
+//75
+fun contributeTimeToEducation() {
+    println("Contribute time to education in Haiti!")
+}
+
+//76
+fun donateSolarPanels() {
+    println("Donate solar panels to Haiti!")
+}
+
+//77
+fun donateMedicalEquipment() {
+    println("Donate medical equipment to Haiti!")
+}
+
+//78
+fun helpDigWells() {
+    println("Help dig wells in Haiti!")
+}
+
+//79
+fun donateSeeds() {
+    println("Donate seeds to Haitian farmers!")
+}
+
+//80
+fun donateTools() {
+    println("Donate tools to help rebuild Haiti!")
+}
+
+//81
+fun donateVehicles() {
+    println("Donate vehicles to transportation in Haiti!")
+}
+
+//82
+fun donateConstructionMaterials() {
+    println("Donate construction materials to rebuild Haiti!")
+}
+
+//83
+fun contributeToResearchProjects() {
+    println("Contribute to research projects on Haiti!")
+}
+
+//84
+fun sponsorAFamily() {
+    println("Sponsor a family in Haiti!")
+}
+
+//85
+fun donateMusicalInstruments() {
+    println("Donate musical instruments to Haiti!")
+}
+
+//86
+fun donateBooksAndMagazines() {
+    println("Donate books and magazines to Haiti!")
+}
+
+//87
+fun provideTranslationServices() {
+    println("Provide translation services to Haiti!")
+}
+
+//88
+fun donateArtSupplies() {
+    println("Donate art supplies to Haiti!")
+}
+
+//89
+fun donateSchoolSupplies() {
+    println("Donate school supplies to Haiti!")
+}
+
+//90
+fun donateToys() {
+    println("Donate toys to Haiti!")
+}
+
+//91
+fun donateSportingEquipment() {
+    println("Donate sporting equipment to Haiti!")
+}
+
+//92
+fun provideVocationalTraining() {
+    println("Provide vocational training to Haiti!")
+}
+
+//93
+fun donateKitchenEquipment() {
+    println("Donate kitchen equipment to Haiti!")
+}
+
+//94
+fun createOpportunitiesForYouth() {
+    println("Create opportunities for youth in Haiti!")
+}
+
+//95
+fun contributeToHackathons() {
+    println("Contribute to hackathons in Haiti!")
+}
+
+//96
+fun provideMentorship() {
+    println("Provide mentorship to Haitians!")
+}
+
+//97
+fun helpDevelopNewTechnologies() {
+    println("Help develop new technologies in Haiti!")
+}
+
+//98
+fun supportCreativeInitiatives() {
+    println("Support creative initiatives in Haiti!")
+}
+
+//99
+fun donateToWomenOrganizations() {
+    println("Donate to women organizations in Haiti!")
+}
+
+//100
+fun donateToLGBTQOrganizations() {
+    println("Donate to LGBTQ organizations in Haiti!")
+}
+
+//101
+fun promoteGenderEquality() {
+    println("Promote gender equality in Haiti!")
+}
+
+//102
+fun donateToMinorityGroups() {
+    println("Donate to minority groups in Haiti!")
+}
+
+//103
+fun donateToEnvironmentalOrganizations() {
+    println("Donate to environmental organizations in Haiti!")
+}
+
+//104
+fun supportClimateChangeInitiatives() {
+    println("Support climate change initiatives in Haiti!")
+}
+
+//105
+fun donateToAnimalRescueOrganizations() {
+    println("Donate to animal rescue organizations in Haiti!")
+}
+
+//106
+fun donateToVeterinaryOrganizations() {
+    println("Donate to veterinary organizations in Haiti!")
+}
+
+//107
+fun donateToDisasterReliefOrganizations() {
+    println("Donate to disaster relief organizations in Haiti!")
+}
+
+//108
+fun volunteerAtLocalEvents() {
+    println("Volunteer at local events in Haiti!")
+}
+
+//109
+fun donateToShelters() {
+    println("Donate to shelters in Haiti!")
+}
+
+//110
+fun donateToOrphanages() {
+    println("Donate to orphanages in Haiti!")
+}
+
+//111
+fun contributeToHumanitarianInitiatives() {
+    println("Contribute to humanitarian initiatives in Haiti!")
+}
+
+//112
+fun provideFinancialLiteracyTraining() {
+    println("Provide financial literacy training in Haiti!")
+}
+
+//113
+fun donateToRefugeeOrganizations() {
+    println("Donate to refugee organizations in Haiti!")
+}
+
+//114
+fun donateToEnvironmentalOrganizations() {
+    println("Donate to environmental organizations in Haiti!")
+}
+
+//115
+fun provideTrainingInRenewableEnergy() {
+    println("Provide training in renewable energy in Haiti!")
+}
+
+//116
+fun donateBuildingMaterials() {
+    println("Donate building materials to Haiti!")
+}
+
+//117
+fun provideTechnicalAssistance() {
+    println("Provide technical assistance in Haiti!")
+}
+
+//118
+fun contributeArticlesAndResearch() {
+    println("Contribute articles and research to Haiti!")
+}
+
+//119
+fun provideCareerAdvice() {
+    println("Provide career advice to Haitians!")
+}
+
+//120
+fun provideEntrepreneurialAdvisoryServices() {
+    println("Provide entrepreneurial advisory services in Haiti!")
+}
+
+//121
+fun participateInProfessionalNetworkingEvents() {
+    println("Participate in professional networking events in Haiti!")
+}
+
+//122
+fun addValueToOrganizations() {
+    println("Add value to organizations working in Haiti!")
+}
+
+//123
+fun provideMarketingServices() {
+    println("Provide marketing services to Haitian organizations!")
+}
+
+//124
+fun joinLinkedInGroups() {
+    println("Join LinkedIn groups to connect with Haitians!")
+}
+
+//125
+fun supportStartupCommunities() {
+    println("Support startup communities in Haiti!")
+}
+
+//126
+fun joinGlobalNetworks() {
+    println("Join global networks that support Haiti!")
+}
+
+//127
+fun writeOpEds() {
+    println("Write op-eds about Haiti!")
+}
+
+//128
+fun createContentAboutHaiti() {
+    println("Create content about Haiti!")
+}
+
+//129
+fun createDisplayAds() {
+    println("Create display ads about Haiti!")
+}
+
+//130
+fun createVideosAboutHaiti() {
+    println("Create videos about Haiti!")
+}
+
+//131
+fun contributeBlogsAboutHaiti() {
+    println("Contribute blogs about Haiti!")
+}
+
+//132
+fun promoteHaitiOn
