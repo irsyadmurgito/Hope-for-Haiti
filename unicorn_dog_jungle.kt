d04039c7027237a1bd063b194f1627b0fe75c641
irsyadmurgito <136702794+irsyadmurgito@users.noreply.github.com>
2023-07-17 20:21:28 +0700

    Merge pull request

---
+++ unicorn_dog_jungle.kt
@@ -0,0 +1,185 @@
+fun main(args: Array<String>) {
+    // Print 'Hope for Haiti'
+    println("Hope for Haiti")
+    
+    // 1. Make a donation
+    val donationAmount = 25.00 
+    println("I'm donating $donationAmount to support Haiti.")
+    
+    // 2. Raise awareness
+    println("I'm raising awareness about the urgent need in Haiti.")
+    
+    // 3. Volunteer
+    println("I'm volunteer to help rebuild Haiti's infrastructure.")
+    
+    // 4. Spread the word
+    println("I'm spreading the word about the need in Haiti by talking to those around me.")
+    
+    // 5. Send a letter
+    println("I'm sending a letter to representatives in my local government to urge them to take action and help.") 
+    
+    // 6. Follow a charity
+    println("I'm following the work of a charity that supports development in Haiti.")
+    
+    // 7. Donate supplies
+    println("I'm donating supplies to those in need in Haiti.")
+    
+    // 8. Get involved
+    println("I'm getting involved in Haiti-related campaigns and events.")
+    
+    // 9. Crowdfund
+    println("I'm crowdfunding to support a project or initiative in Haiti.")
+    
+    // 10. Sign a petition
+    println("I'm signing a petition to support a cause related to Haiti.")
+    
+    // 11. Spread the message on social media
+    println("I'm spreading the message about the need in Haiti on social media.")
+    
+    // 12. Talk to your friends
+    println("I'm talking to my friends about ways that they can help Haiti.")
+    
+    // 13. Visit Haiti
+    println("I'm visiting Haiti to experience the culture and see how I can help.")
+    
+    // 14. Make a commitment
+    println("I'm making a commitment to take action to help Haiti.")
+    
+    // 15. Contact an organization
+    println("I'm contacting an organization that supports Haiti to learn more about how I can help.")
+    
+    // 16. Write a blog
+    println("I'm writing a blog to share my experiences and to encourage others to get involved.")
+    
+    // 17. Become an advocate
+    println("I'm becoming an advocate for Haiti and the people who live there.")
+    
+    // 18. Donate your time
+    println("I'm donating my time to help those in need in Haiti.")
+    
+    // 19. Collaborate with others
+    println("I'm collaborating with others to create sustainable solutions to the problems in Haiti.")
+    
+    // 20. Buy Haitian products
+    println("I'm buying products made in Haiti to support the economy.")
+    
+    // 21. Bring your skills
+    println("I'm bringing my skills to help support the development of Haiti.")
+    
+    // 22. Sponsor a child
+    println("I'm sponsoring a child in Haiti so they can receive an education.")
+    
+    // 23. Teach a class
+    println("I'm teaching a class in Haiti to help people gain new skills.")
+    
+    // 24. Make a video
+    println("I'm making a video to spread awareness about the need in Haiti.")
+    
+    // 25. Participate in events
+    println("I'm participating in events that are focused on Haiti.")
+    
+    // 26. Support a cause
+    println("I'm supporting a cause that is focused on helping Haiti.")
+    
+    // 27. Donate clothes
+    println("I'm donating clothes to those in need in Haiti.")
+    
+    // 28. Write an article
+    println("I'm writing an article about Haiti and the need for support.")
+    
+    // 29. Promote a fundraiser
+    println("I'm promoting a fundraiser to support Haiti.")
+    
+    // 30. Participate in a rally
+    println("I'm participating in a rally to urge others to take action for Haiti.")
+    
+    // 31. Make a plan
+    println("I'm making a plan to help support Haiti in the long term.")
+    
+    // 32. Create a fundraiser
+    println("I'm creating a fundraiser to support a cause in Haiti.")
+    
+    // 33. Become an ambassador
+    println("I'm becoming an ambassador for Haiti and the people who live there.")
+    
+    // 34. Help an organization
+    println("I'm helping an organization that works in Haiti to reach their goals.")
+    
+    // 35. Offer a skill
+    println("I'm offering my skills to help those in Haiti.")
+    
+    // 36. Educate yourself
+    println("I'm educating myself about Haiti and the challenges they face.")
+    
+    // 37. Host an event
+    println("I'm hosting an event to raise awareness and funds for Haiti.")
+    
+    // 38. Share a story
+    println("I'm sharing a story about someone in Haiti to help spread awareness.")
+    
+    // 39. Donate books
+    println("I'm donating books to help students in Haiti learn.")
+    
+    // 40. Donate services
+    println("I'm donating services to help support people in Haiti.")
+    
+    // 41. Research an issue
+    println("I'm researching an issue related to Haiti to help find a solution.")
+    
+    // 42. Connect with a student
+    println("I'm connecting with a student from Haiti to learn about their culture and challenges they face.")
+    
+    // 43. Provide access to education
+    println("I'm providing access to education for students in Haiti.")
+    
+    // 44. Challenge yourself
+    println("I'm challenging myself to help the people of Haiti in any way I can.")
+    
+    // 45. Take a course
+    println("I'm taking a course to learn more about Haiti and how I can help.")
+    
+    // 46. Start a petition
+    println("I'm starting a petition to support a cause related to Haiti.")
+    
+    // 47. Talk to your representatives
+    println("I'm talking to my representatives to urge them to take action and help Haiti.")
+    
+    // 48. Learn a language
+    println("I'm learning a language spoken in Haiti so I can better communicate with the people there.")
+    
+    // 49. Attend a conference
+    println("I'm attending a conference to learn more about Haiti and to network with those who are helping.")
+    
+    // 50. Support local businesses
+    println("I'm supporting local businesses in Haiti to help support the economy.")
+    
+    // 51. Send a care package
+    println("I'm sending a care package to those in need in Haiti.")
+    
+    // 52. Join an organization
+    println("I'm joining an organization that focuses on helping Haiti.")
+    
+    // 53. Help with disaster relief
+    println("I'm helping with disaster relief efforts in Haiti.")
+    
+    // 54. Vote
+    println("I'm voting to support candidates that will help the people of Haiti.")
+    
+    // 55. Organize a project
+    println("I'm organizing a project to help bring resources to Haiti.")
+    
+    // 56. Work with a community
+    println("I'm working with a community in Haiti to develop sustainable solutions.")
+    
+    // 57. Take a trip
+    println("I'm taking a trip to Haiti to volunteer and learn more about the challenges they face.")
+    
+    // 58. Donate a car
+    println("I'm donating a car to help those in need in Haiti.")
+    
+    // 59. Make a connection
+    println("I'm making a connection between someone from Haiti and someone from my community.")
+    
+    // 60. Ask for help
+    println("I'm asking for help to support initiatives in Haiti.") 
+}
